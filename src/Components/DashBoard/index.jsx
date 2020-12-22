5aaf310e8a6eb14252d9bb0117fa1a25f00e7e1d
unknown <tuanbinh.35@gmail.com>
2020-12-22 18:56:24 +0200

    change admin page

--- src/Components/DashBoard/index.jsx
+++ src/Components/DashBoard/index.jsx
@@ -8,8 +8,8 @@ import { makeStyles } from "@material-ui/core/styles";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
-import UserControl from "../UserControl/index"
-import CourseManagement from "../CourseManagement/index"
+import UserControl from "../UserControl/index";
+import CourseManagement from "../CourseManagement/index";
 
 import "./index.scss";
 
@@ -49,61 +49,59 @@ function a11yProps(index) {
 const DashBoard = () => {
   const [value, setValue] = React.useState(0);
 
-  const handleChange = (event,newValue) => {
+  const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
     <>
       <Box className="dashBoard">
-        <Row>
-          <Col xs="12" lg="3" md="3">
-            <Box className="dashBoard__left">
-              <Box className="dashBoard__user">
-                <AccountCircleIcon className="dashBoard__icon" />
-                <h3>Dang Tuan Binh</h3>
-                <p>Senior Admin</p>
-              </Box>
-
-              <Box>
-                <Box className="dashBoard__navigation">
-                <Tabs
-                    orientation="vertical"
-                    value={value}
-                    onChange={handleChange}
-                    // aria-label="Vertical tabs example"
-                  >
-                    <Tab label="User Control" {...a11yProps(0)} />
-                    <Tab label="Course Management" {...a11yProps(1)} />
-                    <Tab label="Pending Course" {...a11yProps(2)} />
-                    <Tab label="Adding Course" {...a11yProps(3)} />
-                  </Tabs>
+          <Row>
+            <Col xs="12" lg="3" md="3">
+              <Box className="dashBoard__left">
+                <Box className="dashBoard__user">
+                  <AccountCircleIcon className="dashBoard__icon" />
+                  <h3>Dang Tuan Binh</h3>
+                  <p>Senior Admin</p>
                 </Box>
-                <Box className="dashBoard__logOut">
-                  <span>Sign out</span>
-                  <ExitToAppIcon />
+
+                <Box>
+                  <Box className="dashBoard__navigation">
+                    <Tabs
+                      orientation="vertical"
+                      value={value}
+                      onChange={handleChange}
+                    >
+                      <Tab label="User Control" {...a11yProps(0)} />
+                      <Tab label="Course Management" {...a11yProps(1)} />
+                      <Tab label="Pending Course" {...a11yProps(2)} />
+                      <Tab label="Adding Course" {...a11yProps(3)} />
+                    </Tabs>
+                  </Box>
+                  <Box className="dashBoard__logOut">
+                    <span>Sign out</span>
+                    <ExitToAppIcon />
+                  </Box>
                 </Box>
               </Box>
-            </Box>
-          </Col>
-
-          <Col xs="12" lg="9" md="9">
-            <Box className="dashBoard__right">
-              <TabPanel value={value} index={0}>
-                <UserControl />
-              </TabPanel>
-              <TabPanel value={value} index={1}>
-                <CourseManagement />
-              </TabPanel>
-              <TabPanel value={value} index={2}>
-                Item Three
-              </TabPanel>
-              <TabPanel value={value} index={3}>
-                Item Four
-              </TabPanel>
-            </Box>
-          </Col>
-        </Row>
+            </Col>
+            <Col xs="12" lg="9" md="9">
+              <Box className="dashBoard__right">
+                <TabPanel value={value} index={0}>
+                  <UserControl />
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                  <CourseManagement />
+                </TabPanel>
+                <TabPanel value={value} index={2}>
+                  Item Three
+                </TabPanel>
+                <TabPanel value={value} index={3}>
+                  Item Four
+                </TabPanel>
+              </Box>
+            </Col>
+          </Row>
       </Box>
     </>
   );
